685da5afd3ed540ca59d3d183d6f0b2d580afb27
Egliott <49058912+egliott@users.noreply.github.com>
2025-11-19 09:54:08 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    """
+    Entry point of the application.
+    - Initialize logging.
+    - Initialize config.
+    - Initialize STT engine (load models).
+    - Launch GUI.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
